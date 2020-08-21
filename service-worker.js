689e50e0eeb0b7f10222b0c4bd31728b7bea18f5
Lucas Alves <lucasvra@ufmg.br>
2020-08-20 23:10:02 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,9 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.dat$/ ];
-const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+const offlineAssetsExclude = [/^service-worker\.js$/];
+
+console.log('Cache Name: ' + cacheName);
 
 async function onInstall(event) {
     console.info('Service worker: Install');
@@ -46,4 +48,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: iofUorpo */
+/* Manifest version: pDQapcVf */
